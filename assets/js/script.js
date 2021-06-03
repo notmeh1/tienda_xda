2a10197f6daf231816a6a9c6dcbc6308895bc160
Andres <zleckzxd@gmail.com>
2021-06-03 17:15:35 -0400

    + avances
    navbar, posicionamiento y más

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("master").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("master").style.marginLeft= "0";
+  }
